dd612735a68ba82c261d9e7695f164f3c2d2eac2
Roopesh K <rkottala@mcfadyen.com>
2024-12-31 22:41:21 +0530

    aa

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      Customrs 11 <a href="/dashboard">Home</a>
+    </div>
+  );
+}
